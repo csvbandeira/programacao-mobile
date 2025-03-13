dfe5fa6b968b76afa7025822cfa186b23598d9d2
csvbandeira <csbandeira18@gmail.com>
2025-03-13 19:56:10 -0300

    desafio 04

--- myApp/App.js
+++ myApp/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-function App() {
+const App = () => {
   return (
-    <View style={{ backgroundColor: "blue" }}>
-      <View style={{ backgroundColor: "yellow" }}>
+    <View style={{ backgroundColor: "blue", height: 300 }}>
+      <View style={{ backgroundColor: "yellow", height: 150 }}>
         <Text></Text>
         <Text>Componente View</Text>
         <Text>Carlos Victor Bandeira</Text>
         <Text>13/03/2025</Text>
       </View>
-      <View style={{ backgroundColor: "red" }}>
+      <View style={{ backgroundColor: "red", height: 75 }}>
         <Text>Componente View</Text>
         <Text>Carlos Victor Bandeira</Text>
         <Text>13/03/2025</Text>
       </View>
     </View>
   );
-}
+};
 
 export default App;
