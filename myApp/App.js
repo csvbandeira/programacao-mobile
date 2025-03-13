c2a43ba27cb25a2bad83b4ac644959754748ad4f
csvbandeira <csbandeira18@gmail.com>
2025-03-13 20:15:27 -0300

    desafio 05

--- myApp/App.js
+++ myApp/App.js
@@ -3,14 +3,19 @@ import { View, Text } from "react-native";
 
 const App = () => {
   return (
-    <View style={{ backgroundColor: "blue", height: 300 }}>
-      <View style={{ backgroundColor: "yellow", height: 150 }}>
+    <View style={{ backgroundColor: "blue", height: "100%", width: 450 }}>
+      <View style={{ width: "100%", height: "20%", backgroundColor: "green" }}>
         <Text></Text>
         <Text>Componente View</Text>
         <Text>Carlos Victor Bandeira</Text>
         <Text>13/03/2025</Text>
       </View>
-      <View style={{ backgroundColor: "red", height: 75 }}>
+      <View style={{ backgroundColor: "yellow", height: "70%", width: 100 }}>
+        <Text>Componente View</Text>
+        <Text>Carlos Victor Bandeira</Text>
+        <Text>13/03/2025</Text>
+      </View>
+      <View style={{ backgroundColor: "red", height: "10%", width: 300 }}>
         <Text>Componente View</Text>
         <Text>Carlos Victor Bandeira</Text>
         <Text>13/03/2025</Text>
