287d6e98e5913791b3e0d9b8e45cf8ea61c6833b
csvbandeira <csbandeira18@gmail.com>
2025-03-13 19:03:11 -0300

    desafio 02

--- myApp/App.js
+++ myApp/App.js
@@ -4,9 +4,16 @@ import { View, Text } from "react-native";
 const App = () => {
   return (
     <View>
-      <Text>Componente View</Text>
-      <Text>Carlos Victor Bandeira</Text>
-      <Text>13/03/2025</Text>
+      <View>
+        <Text>Componente View</Text>
+        <Text>Carlos Victor Bandeira</Text>
+        <Text>13/03/2025</Text>
+      </View>
+      <View>
+        <Text>Componente View</Text>
+        <Text>Carlos Victor Bandeira</Text>
+        <Text>13/03/2025</Text>
+      </View>
     </View>
   );
 };
