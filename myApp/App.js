7c29702575a3af50a54cf8854b2868c0d9118c85
csvbandeira <csbandeira18@gmail.com>
2025-04-01 13:06:24 -0300

    desafio 01

--- myApp/App.js
+++ myApp/App.js
@@ -3,112 +3,184 @@ import { View, Text, Image } from "react-native";
 
 function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "orange", margin: 10 }}>
+    <View
+      style={{
+        height: "100%",
+        width: "100%",
+        padding: 10,
+        backgroundColor: "orange",
+      }}
+    >
       <View
         style={{
-          alignItems: "center",
-          backgroundColor: "skyblue",
+          display: "flex",
           flexDirection: "column",
-          paddingVertical: 20,
-        }}
-      >
-        <Text style={{ fontSize: 30, color: "blue", marginBottom: 20 }}>
-          Componente Image
-        </Text>
-        <Image
-          source={require("./assets/img/img1.png")}
-          style={{
-            width: 200,
-            height: 100,
-            borderColor: "black",
-            borderWidth: 3,
-          }}
-        />
-      </View>
-
-      <View
-        style={{
-          alignItems: "center",
-          backgroundColor: "#5f9ea0",
-          flexDirection: "row",
-          paddingVertical: 20,
-          paddingHorizontal: 10,
-          justifyContent: "space-around",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 20,
-            color: "white",
-            marginBottom: 20,
-            width: "60%",
-            textAlign: "justify",
-          }}
-        >
-          O react Native pode estilizar o componente Image de várias formas:
-        </Text>
-        <Image
-          source={require("./assets/img/img1.png")}
-          style={{
-            width: 100,
-            height: 100,
-            borderColor: "green",
-            borderWidth: 5,
-            borderRadius: 50,
-          }}
-        />
-      </View>
-
-      <View
-        style={{
-          alignItems: "center",
-          backgroundColor: "#dcdcdc",
-          flexDirection: "row",
-          paddingVertical: 20,
-          paddingHorizontal: 10,
-          justifyContent: "space-around",
-        }}
-      >
-        <Image
-          source={require("./assets/img/img2.png")}
-          style={{
-            width: 100,
-            height: 100,
-            borderColor: "white",
-            borderWidth: 2,
-            borderTopLeftRadius: 50,
-            marginHorizontal: 20,
-          }}
-        />
-        <Text
-          style={{
-            fontSize: 15,
-            color: "gray",
-            marginBottom: 20,
-            width: "70%",
-            textAlign: "justify",
-          }}
-        >
-          - Alterando o formato e a cor da borda;{"\n"}- Alterando as dimensões
-          e o posicionamento;{"\n"}- Alterando margens e espaçamentos, etc...
-        </Text>
-      </View>
-      <View
-        style={{
+          justifyContent: "flex-end",
+          height: "100%",
           width: "100%",
-          height: "absolute",
-          bottom: 15,
-          alignItems: "center",
-          backgroundColor: "white",
-          padding: 20,
+          backgroundColor: "blue",
+          borderRadius: 25,
         }}
       >
-        <Image
-          source={{ uri: "https://izap.com.br/uploads/google-logoblog.png" }}
-          style={{ width: 200, height: 100 }}
-        />
-        <View style={{ flexDirection: "row" }}>
-          Carlos Victor Bandeira - 2025
+        <View
+          style={{
+            height: "20%",
+            width: "100%",
+            padding: 20,
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <View
+            style={{
+              height: "50%",
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Image
+              style={{ height: "50%", width: "10%" }}
+              source={require("./assets/img/user.png")}
+            />
+            <Image
+              style={{ height: "50%", width: "10%" }}
+              source={require("./assets/img/notification.png")}
+            />
+          </View>
+          <View style={{ height: "50%", width: "100%" }}>
+            <Text style={{ color: "white" }}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: "80%",
+            width: "100%",
+            backgroundColor: "white",
+            padding: 20,
+            borderRadius: 25,
+            gap: 25,
+          }}
+        >
+          <View
+            style={{
+              height: "20%",
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignContent: "center",
+              marginTop: "50px",
+              flexWrap: "wrap",
+            }}
+          >
+            <Text
+              style={{
+                width: "100%",
+                marginBottom: 10,
+                color: "blue",
+                fontWeight: "bold",
+                fontSize: 20,
+              }}
+            >
+              Categories
+            </Text>
+            <Image
+              style={{ height: "75%", width: "25%", borderRadius: 15 }}
+              source={require("./assets/img/img1.png")}
+            />
+            <Image
+              style={{ height: "75%", width: "25%", borderRadius: 15 }}
+              source={require("./assets/img/img2.png")}
+            />
+            <Image
+              style={{ height: "75%", width: "25%", borderRadius: 15 }}
+              source={require("./assets/img/img3.png")}
+            />
+          </View>
+          <View
+            style={{
+              height: "80%",
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignContent: "center",
+              marginTop: "50px",
+              flexWrap: "wrap",
+            }}
+          >
+            <Text
+              style={{
+                width: "100%",
+                marginBottom: 10,
+                color: "blue",
+                fontWeight: "bold",
+                fontSize: 20,
+              }}
+            >
+              Technology
+            </Text>
+            <View
+              style={{
+                height: "30%",
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Image
+                style={{ height: "75%", width: "40%", borderRadius: 15 }}
+                source={require("./assets/img/img1.png")}
+              />
+              <Text style={{ height: "75%", width: "55%" }}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </Text>
+            </View>
+            <View
+              style={{
+                height: "30%",
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Image
+                style={{ height: "75%", width: "40%", borderRadius: 15 }}
+                source={require("./assets/img/img1.png")}
+              />
+              <Text style={{ height: "75%", width: "55%" }}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </Text>
+            </View>
+            <View
+              style={{
+                height: "30%",
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Image
+                style={{ height: "75%", width: "40%", borderRadius: 15 }}
+                source={require("./assets/img/img1.png")}
+              />
+              <Text style={{ height: "75%", width: "55%" }}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </Text>
+            </View>
+          </View>
         </View>
       </View>
     </View>
