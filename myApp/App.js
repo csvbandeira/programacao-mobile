d87139d9cbb41fb25e3b7664cb4554fb0fe82136
csvbandeira <csbandeira18@gmail.com>
2025-04-06 22:13:10 -0300

    desafio 01

--- myApp/App.js
+++ myApp/App.js
@@ -1,180 +1,96 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 
-function App() {
-  const content =
-    " Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+const App = () => {
+  const drivers = [
+    { position: 1, name: "Max Verstappen", bestTime: "01:11:09" },
+    { position: 2, name: "Max Verstappen", bestTime: "01:11:45" },
+    { position: 3, name: "Max Verstappen", bestTime: "01:11:99" },
+    { position: 4, name: "Max Verstappen", bestTime: "01:12:12" },
+    { position: 5, name: "Max Verstappen", bestTime: "01:13:01" },
+  ];
+
+  let Content = null;
+
+  for (let i = 0; i < drivers.length; i++) {
+    Content = (
+      <>
+        {Content}
+        <View
+          key={i}
+          style={{
+            height: "19%",
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-around",
+            padding: 10,
+            backgroundColor: "white",
+            borderRadius: 20,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 24,
+              fontWeight: "semi-bold",
+            }}
+          >
+            {drivers[i].position}
+          </Text>
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+            {drivers[i].name.toUpperCase()}
+          </Text>
+          <Text style={{ fontSize: 16, fontWeight: "semi-bold" }}>
+            {drivers[i].bestTime}
+          </Text>
+        </View>
+      </>
+    );
+  }
+
   return (
-    <View
-      style={{
-        height: "100%",
-        width: "100%",
-        padding: 10,
-        backgroundColor: "orange",
-      }}
-    >
+    <View style={{ height: "100%", width: "100%" }}>
       <View
         style={{
+          height: "20%",
+          width: "100%",
+          backgroundColor: "black",
+          paddingVertical: 20,
+          paddingHorizontal: 10,
           display: "flex",
           flexDirection: "column",
-          justifyContent: "flex-end",
-          height: "100%",
-          width: "100%",
-          backgroundColor: "blue",
-          borderRadius: 25,
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <View
-          style={{
-            height: "20%",
-            width: "100%",
-            padding: 20,
-            display: "flex",
-            justifyContent: "space-around",
-          }}
-        >
-          <View
-            style={{
-              height: "50%",
-              width: "100%",
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Image
-              style={{ height: "50%", width: "10%" }}
-              source={require("./assets/img/user.png")}
-            />
-            <Image
-              style={{ height: "50%", width: "10%" }}
-              source={require("./assets/img/notification.png")}
-            />
-          </View>
-          <View style={{ height: "50%", width: "100%" }}>
-            <Text style={{ color: "white" }}>{content}</Text>
-          </View>
-        </View>
-        <View
-          style={{
-            height: "80%",
-            width: "100%",
-            backgroundColor: "white",
-            padding: 20,
-            borderRadius: 25,
-            gap: 25,
-          }}
-        >
-          <View
-            style={{
-              height: "20%",
-              width: "100%",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignContent: "center",
-              marginTop: "50px",
-              flexWrap: "wrap",
-            }}
-          >
-            <Text
-              style={{
-                width: "100%",
-                marginBottom: 10,
-                color: "blue",
-                fontWeight: "bold",
-                fontSize: 20,
-              }}
-            >
-              Categories
-            </Text>
-            <Image
-              style={{ height: "75%", width: "25%", borderRadius: 15 }}
-              source={require("./assets/img/img1.png")}
-            />
-            <Image
-              style={{ height: "75%", width: "25%", borderRadius: 15 }}
-              source={require("./assets/img/img2.png")}
-            />
-            <Image
-              style={{ height: "75%", width: "25%", borderRadius: 15 }}
-              source={require("./assets/img/img3.png")}
-            />
-          </View>
-          <View
-            style={{
-              height: "80%",
-              width: "100%",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignContent: "center",
-              marginTop: "50px",
-              flexWrap: "wrap",
-            }}
-          >
-            <Text
-              style={{
-                width: "100%",
-                marginBottom: 10,
-                color: "blue",
-                fontWeight: "bold",
-                fontSize: 20,
-              }}
-            >
-              Technology
-            </Text>
-            <View
-              style={{
-                height: "30%",
-                width: "100%",
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
-            >
-              <Image
-                style={{ height: "75%", width: "40%", borderRadius: 15 }}
-                source={require("./assets/img/img1.png")}
-              />
-              <Text style={{ height: "75%", width: "55%" }}>{content}</Text>
-            </View>
-            <View
-              style={{
-                height: "30%",
-                width: "100%",
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
-            >
-              <Image
-                style={{ height: "75%", width: "40%", borderRadius: 15 }}
-                source={require("./assets/img/img1.png")}
-              />
-              <Text style={{ height: "75%", width: "55%" }}>{content}</Text>
-            </View>
-            <View
-              style={{
-                height: "30%",
-                width: "100%",
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-              }}
-            >
-              <Image
-                style={{ height: "75%", width: "40%", borderRadius: 15 }}
-                source={require("./assets/img/img1.png")}
-              />
-              <Text style={{ height: "75%", width: "55%" }}>{content}</Text>
-            </View>
-          </View>
-        </View>
+        <Image
+          source={require("./assets/img/formula-1.png")}
+          style={{ height: "100%", width: "75%", borderRadius: 20 }}
+        />
       </View>
+      <View
+        style={{
+          height: "70%",
+          width: "100%",
+          backgroundColor: "red",
+          margin: "auto",
+          padding: 10,
+          gap: 5,
+        }}
+      >
+        {Content}
+      </View>
+      <View
+        style={{
+          height: "10%",
+          width: "100%",
+          backgroundColor: "black",
+          padding: 10,
+        }}
+      ></View>
     </View>
   );
-}
+};
 
 export default App;
