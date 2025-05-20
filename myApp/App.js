e3f56f449f3719945f6cdfaa26623e76a6daa1a8
csvbandeira <csbandeira18@gmail.com>
2025-05-19 22:57:15 -0300

    removendo useState não utilizado

--- myApp/App.js
+++ myApp/App.js
@@ -1,20 +1,9 @@
-import { useState } from "react";
 import { Text, View } from "react-native";
 
 import Body from "./src/components/Body";
 import { Styles } from "./src/styles/style";
 
 function App() {
-	const [saudacao, setSaudacao] = useState("");
-
-	function capturaNome(texto_digitado) {
-		if (texto_digitado.length > 0) {
-			setSaudacao("olá " + texto_digitado + "\nSeja bem vindo!");
-		} else {
-			setSaudacao("");
-		}
-	}
-
 	return (
 		<View style={Styles.area_fundo}>
 			<View style={Styles.cabecalho}>
