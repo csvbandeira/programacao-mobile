0b82f66411526289d99ce724999d2489750e5b03
csvbandeira <csbandeira18@gmail.com>
2025-06-02 17:59:25 -0300

    arrumando detalhe

--- myApp/src/components/Body.js
+++ myApp/src/components/Body.js
@@ -1,9 +1,11 @@
 import { estilos } from "../styles/style";
 
+import { useState } from "react";
 import { View, Text, TextInput, TouchableHighlight } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
 const Body = () => {
+	const [color, setColor] = useState("#ffff00");
 	const content =
 		"No mundo do React Native, o componente Picker (selecionador) " +
 		"se refere a um componente que permite ao usuário escolher entre um conjunto de opções. \n \n" +
@@ -11,11 +13,18 @@ const Body = () => {
 
 	const verify = () => alert("ola");
 
+	const focused = () => setColor("orange");
+	const blur = () => setColor("#ffff00");
+
 	return (
 		<View id="conteudo" style={estilos.conteudo}>
 			<Text style={estilos.textoConteudo}>{content}</Text>
 			<Text style={estilos.textoConteudo}>Digite seu nome:</Text>
-			<TextInput style={estilos.entradaTextoConteudo} />
+			<TextInput
+				style={[estilos.entradaTextoConteudo, { backgroundColor: color }]}
+				onFocus={focused}
+				onBlur={blur}
+			/>
 			<Text style={estilos.textoConteudo}>Escolha uma das opções abaixo:</Text>
 
 			<Picker style={{ backgroundColor: "#1e90ff", height: 50 }}>
