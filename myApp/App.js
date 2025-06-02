edc002124f8d7a17b758133b2b1e059f21d4cb23
csvbandeira <csbandeira18@gmail.com>
2025-06-02 16:43:30 -0300

    picker desafio 01

--- myApp/App.js
+++ myApp/App.js
@@ -1,19 +1,62 @@
-import { Text, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { Text, View, Image, TextInput, TouchableHighlight } from "react-native";
 
-import Body from "./src/components/Body";
-import { Styles } from "./src/styles/style";
+import { estilos } from "./src/styles/style";
 
-function App() {
+const App = () => {
+	let logo2 = require("./src/img/logo2.png");
+	let logo1 = require("./src/img/logo1.png");
+	let textoConteudo =
+		"No mundo do React Native, o componente Picker (selecionador) " +
+		"se refere a um componente que permite ao usuário escolher entre um conjunto de opções. \n \n" +
+		"Verifique o Exemplo abaixo:";
+
+	const verifica = () => alert("ola");
+
 	return (
-		<View style={Styles.area_fundo}>
-			<View style={Styles.cabecalho}>
-				<Text style={Styles.cabTitulo}>Aula 06</Text>
-				<Text style={Styles.cabSubTitulo}>Componente TextInput</Text>
+		<View style={estilos.area_fundo}>
+			<View id="cabecalho" style={estilos.cabecalho}>
+				<Image source={logo1} style={estilos.imgCabecalho} />
+				<Text style={estilos.textoCabecalho}>Componente Picker</Text>
 			</View>
 
-			<Body />
+			<View id="conteudo" style={estilos.conteudo}>
+				<Text style={estilos.textoConteudo}>{textoConteudo}</Text>
+				<Text style={estilos.textoConteudo}>Digite seu nome:</Text>
+				<TextInput style={estilos.entradaTextoConteudo} />
+				<Text style={estilos.textoConteudo}>
+					Escolha uma das opções abaixo:
+				</Text>
+
+				<Picker style={{ backgroundColor: "#1e90ff", height: 50 }}>
+					<Picker.Item label="Selecione..." value="Selecione" />
+					<Picker.Item label="Linguagem Java" value="Java" />
+					<Picker.Item label="Linguagem JS" value="JS" />
+					<Picker.Item label="Linguagem PHP" value="PHP" />
+				</Picker>
+
+				<TouchableHighlight onPress={verifica} style={estilos.botaoConteudo}>
+					<Text
+						style={{
+							color: "#ffff00",
+							fontWeight: "bold",
+							textAlign: "center",
+						}}
+					>
+						CLIQUE AQUI
+					</Text>
+				</TouchableHighlight>
+			</View>
+
+			<View id="rodape" style={estilos.rodape}>
+				<View style={{ flexDirection: "row", alignItems: "center" }}>
+					<Image source={logo2} style={estilos.imgRodape} />
+					<Text style={estilos.textoRodape}>Luiz Fernando Corcini</Text>
+				</View>
+				<Text style={estilos.textoRodape}>2023</Text>
+			</View>
 		</View>
 	);
-}
+};
 
 export default App;
--- myApp/src/styles/style.js
+++ myApp/src/styles/style.js
@@ -1,67 +1,75 @@
 import { StyleSheet } from "react-native";
 
-export const Styles = StyleSheet.create({
+export const estilos = StyleSheet.create({
 	area_fundo: {
 		flex: 1,
-		paddingHorizontal: 20,
-		marginTop: 25,
+		backgroundColor: "#1e90ff",
 	},
 	cabecalho: {
 		flex: 0.3,
-		backgroundColor: "lightgray",
-		width: "100%",
-		borderRadius: 20,
+		backgroundColor: "#1e90ff",
+		marginTop: 25,
 		justifyContent: "center",
 		alignItems: "center",
 	},
-	cabTitulo: {
+	textoCabecalho: {
 		fontSize: 30,
+		color: "#3fff",
+		fontWeight: "bold",
+	},
+	imgCabecalho: {
+		width: 100,
+		height: 100,
+	},
+	conteudo: {
+		flex: 0.68,
+		justifyContent: "flex-start",
+		paddingHorizontal: 15,
+		paddingVertical: 25,
+		backgroundColor: "#ffff00",
+		borderTopLeftRadius: 25,
+		borderTopRightRadius: 25,
+		marginHorizontal: 10,
+		borderBottomWidth: 2,
+		borderBottomColor: "gray",
+	},
+	textoConteudo: {
+		color: "#1e90ff",
+		fontSize: 18,
+		textAlign: "justify",
+	},
+	entradaTextoConteudo: {
+		height: 45,
+		borderWidth: 1,
+		borderColor: "#222",
+		fontSize: 20,
+		padding: 10,
+		marginBottom: 10,
+	},
+	botaoConteudo: {
+		backgroundColor: "#1e90ff",
+		color: "#ffff00",
 		fontWeight: "bold",
 		marginTop: 20,
-		marginBottom: 10,
-	},
-	cabSubTitulo: {
-		fontSize: 20,
-		marginBottom: 10,
-	},
-	corpo: {
-		flex: 0.5,
-	},
-	paragrafo: {
-		marginTop: 20,
-		padding: 10,
-	},
-	entrada_texto: {
-		height: 45,
-		width: "90%",
-		borderWidth: 1,
-		borderColor: "#222",
-		marginHorizontal: 10,
-		fontSize: 20,
-		padding: 10,
-		marginTop: 10,
-	},
-	entrada_texto2: {
-		height: 100,
-		width: "90%",
-		borderWidth: 1,
-		borderColor: "#222",
-		marginHorizontal: 10,
-		fontSize: 20,
-		padding: 5,
-		marginTop: 10,
+		padding: 20,
+		width: 90,
+		alignSelf: "center",
+		borderRadius: 50,
 	},
 	rodape: {
-		flex: 0.1,
-		backgroundColor: "lightgray",
-		width: "100%",
-		borderRadius: 20,
-		justifyContent: "space-around",
-		alignItems: "flex-end",
+		flex: 0.08,
 		flexDirection: "row",
+		backgroundColor: "#ffff00",
+		justifyContent: "space-around",
+		alignItems: "center",
 	},
-	rodTexto: {
-		fontSize: 15,
-		marginBottom: 10,
+	textoRodape: {
+		fontSize: 20,
+		color: "#1e90ff",
+		fontWeight: "bold",
+	},
+	imgRodape: {
+		width: 50,
+		height: 50,
 	},
 });
