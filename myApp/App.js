6fb90f0e68e0e99d6edd23a323b20bf03762f581
csvbandeira <csbandeira18@gmail.com>
2025-06-02 18:38:48 -0300

    desafio 2 slider

--- myApp/App.js
+++ myApp/App.js
@@ -1,18 +1,81 @@
-import { Text, View, Image, TextInput, TouchableHighlight } from "react-native";
+import { useState } from "react";
+import {
+	Switch,
+	TouchableHighlight,
+	View,
+	Text,
+	StyleSheet,
+	Image,
+} from "react-native";
 
-import { estilos } from "./src/styles/style";
-import Header from "./src/components/Header";
-import Body from "./src/components/Body";
-import Footer from "./src/components/Footer";
+function App() {
+	const [est, setEst] = useState(estilos2);
+	const [ligado, setLigado] = useState(false);
+	const [textobotao, setTextoBotao] = useState("estilo2");
 
-const App = () => {
+	function mudaEstilo() {
+		if (ligado) {
+			setEst(estilos2);
+			setLigado(false);
+			setTextoBotao("estilo 2");
+		} else {
+			setEst(estilos1);
+			setLigado(true);
+			setTextoBotao("estilo 1 ");
+		}
+	}
+
 	return (
-		<View style={estilos.area_fundo}>
-			<Header />
-			<Body />
-			<Footer />
+		<View style={est.area}>
+			<Image source={require("./assets/img/img1.png")} />
+			<Text style={est.font}>COMPONENTE SWITCH</Text>
+			<TouchableHighlight onPress={mudaEstilo} style={est.botao}>
+				<Text style={est.textobotao}>{textobotao}</Text>
+			</TouchableHighlight>
+			<Switch value={ligado} onValueChange={mudaEstilo} />
 		</View>
 	);
-};
+}
+
+const estilos1 = StyleSheet.create({
+	area: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 20,
+	},
+	botao: {
+		padding: 30,
+		backgroundColor: "white",
+		borderWidth: 1,
+		borderColor: "red",
+		borderRadius: 30,
+	},
+	textobotao: {
+		color: "blue",
+		fontSize: 20,
+	},
+	font: {
+		textAlign: "center",
+		fontSize: 40,
+	},
+});
+
+const estilos2 = StyleSheet.create({
+	area: {
+		flex: 1,
+		alignItems: "flex-start",
+		justifyContent: "space-around",
+		backgroundColor: "yellow",
+	},
+	botao: {
+		padding: 20,
+		backgroundColor: "black",
+	},
+	textobotao: {
+		color: "white",
+		fontSize: 30,
+	},
+});
 
 export default App;
